3aec52a8afca830c12b48c69cef16a680b7c4720
JJAJJAKIM <csm9121@gmail.com>
2025-06-05 18:22:16 +0900

    환경설정 02

---
+++ src/main/java/com/news/snapNews/SnapNewsApplication.java
@@ -0,0 +1,13 @@
+package com.news.snapNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/news/snapNews/SnapNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.snapNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
